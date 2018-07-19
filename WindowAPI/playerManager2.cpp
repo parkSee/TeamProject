f87bfd2bbf4dfcbf7fcc6b79bbb6e681f33f7cc1
5_KGA_20-PC\5_KGA_20 <92bulldozer@naver.com>
2018-07-19 12:42:22 +0900

    플레이어 매니저2 추가용

---
+++ WindowAPI/playerManager2.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "playerManager2.h"
+
+
+playerManager2::playerManager2()
+{
+	//오예 생성했다.
+}
+
+
+playerManager2::~playerManager2()
+{
+}
---
+++ WindowAPI/playerManager2.h
@@ -0,0 +1,8 @@
+#pragma once
+class playerManager2
+{
+public:
+	playerManager2();
+	~playerManager2();
+};
+
